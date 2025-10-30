debdeff9f091704172f9838ee4fd22e32f514819
PeculiarMedia <peculiarmedia01@gmail.com>
2025-10-30 01:52:39 +0100

    Update App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import Generate from "./pages/Generate"
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", marginTop: "20px" }}>
+        Social Media Agent
+      </h1>
+      <Generate />
+    </div>
+  )
+}
+
+export default App
