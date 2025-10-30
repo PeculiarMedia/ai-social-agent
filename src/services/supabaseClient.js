7dbd3f2d9f7f7abacace76ed4ab6507d2bea2bb5
PeculiarMedia <peculiarmedia01@gmail.com>
2025-10-30 11:55:03 +0100

    Update supabaseClient.js

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -4,4 +4,6 @@ import { createClient } from '@supabase/supabase-js'
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+export const supabase = createClient(https://gksvudeydddtdclztzuq.supabase.co
+, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdrc3Z1ZGV5ZGRkdGRjbHp0enVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE3MzkwNDcsImV4cCI6MjA3NzMxNTA0N30.TpIhZ0wN-dWL4Pp5g_mmnvYY4kwBq9uoxeWpCJ_oBzs)
+
